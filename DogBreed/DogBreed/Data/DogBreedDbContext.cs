d173a385c00e5442738a64f7412a389dd162f983
Sophie <tengxxu@gmail.com>
2021-07-23 19:32:28 -0400

    Add Model and DbContext

---
+++ DogBreed/DogBreed/Data/DogBreedDbContext.cs
@@ -0,0 +1,19 @@
+﻿using DogBreed.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogBreed.Data
+{
+    public class DogBreedDbContext: DbContext
+    {
+
+        public DogBreedDbContext(DbContextOptions<DogBreedDbContext> options): base(options)
+        {
+        }
+
+        public DbSet<Breed> Breeds { get; set; }
+    }
+}
---
+++ DogBreed/DogBreed/Models/Breed.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogBreed.Models
+{
+    public class Breed
+    {
+        [Key]
+        [HiddenInput]
+        [Required]
+        public int DogID { get; set; }
+
+        public string BreedName { get; set; }
+
+        public string SubBreedName { get; set; }
+    }
+}
