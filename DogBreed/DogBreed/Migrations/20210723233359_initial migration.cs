383198f93d1ef60631e8c0831339acb1ba20225b
Sophie <tengxxu@gmail.com>
2021-07-23 19:36:15 -0400

    Add database and table for model breed

---
+++ DogBreed/DogBreed/Migrations/20210723233359_initial migration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DogBreed.Migrations
+{
+    public partial class initialmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Breeds",
+                columns: table => new
+                {
+                    DogID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BreedName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SubBreedName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Breeds", x => x.DogID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Breeds");
+        }
+    }
+}
