e9ab7f119a0800ceef72009e493680080277d04d
Sophie <tengxxu@gmail.com>
2021-07-25 15:42:30 -0400

    Add user search function

--- DogBreed/DogBreed/Pages/Index.cshtml.cs
+++ DogBreed/DogBreed/Pages/Index.cshtml.cs
@@ -14,10 +14,10 @@ namespace DogBreed.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
-        public string Result
-        {
-            get; set;
-        }
+        //public List<string> ResultList
+        //{
+        //    get; set;
+        //}
 
         public bool ResultSet
         {
@@ -40,9 +40,11 @@ namespace DogBreed.Pages
 
         public IList<Breed> Breeds { get; set; }
 
-        public List<string> breedList = new List<string>();
+        public List<string> BreedList = new List<string>();
 
-        public List<string> subBreedList = new List<string>();
+        public List<string> SubBreedList = new List<string>();
+
+        public List<string> ResultList = new List<string>();
 
 
         public async Task OnPostAsync(string searchValue, string breedType)
@@ -50,20 +52,26 @@ namespace DogBreed.Pages
 
             Breeds = await _context.Breeds.ToListAsync();
 
-            breedList = Breeds.Select(breed => breed.BreedName).ToList();  // The list of all main breed names
-            subBreedList = Breeds.Select(breed => breed.SubBreedName).ToList();  // The list of all sub breed names
-            var allBreedList = breedList.Zip(subBreedList, (breed, subBreed) => $"{breed}{subBreed}");  //The list of all (main breed name + sub breed name)s
+            BreedList = Breeds.Select(breed => breed.BreedName).ToList();  // The list of all main breed names
+            SubBreedList = Breeds.Select(breed => breed.SubBreedName).ToList();  // The list of all sub breed names
+            var AllBreedList = BreedList.Zip(SubBreedList, (breed, subBreed) => $"{breed} {subBreed}");  //The list of all (main breed name + sub breed name)s
 
             switch (breedType)
             {
                 case "both":
-                    foreach (var allBreed in allBreedList) { 
-
+                    // search what user typed in the list
+                    foreach (var AllBreed in AllBreedList)
+                    {
+                        if (AllBreed.Contains(searchValue)){
+                            // TODO upper case and lower case
+                            ResultList.Add(AllBreed);
+                        }
                     }
 
-
-                    Result = $"{breedType}: ";
-                    ResultSet = true;
+                    
+                    if (ResultList.Count != 0) {
+                        ResultSet = true;
+                    }
                     break;
                 case "main_breed":
                     break;
