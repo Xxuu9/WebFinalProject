e98b1bd1bc8aa3c6db3dc71bc381abf2a2103865
Sophie <tengxxu@gmail.com>
2021-07-26 16:16:32 -0400

    Add two unit tests

--- DogBreed/SearchBreeds-Tests/SearchBreedsTest.cs
+++ DogBreed/SearchBreeds-Tests/SearchBreedsTest.cs
@@ -8,7 +8,21 @@ namespace SearchBreeds_Tests
         [TestMethod]
         public void TestMethod1()
         {
-            
+            string string1 = "Dog";
+            string string2 = "D";
+            bool result = SearchBreeds.SearchBreedName.CheckStringContain(string1, string2);
+            Assert.AreEqual(true, result);
+
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string string1 = "Dog";
+            string string2 = "d";
+            bool result = SearchBreeds.SearchBreedName.CheckStringContain(string1, string2);
+            Assert.AreEqual(true, result);
+
         }
     }
 }
