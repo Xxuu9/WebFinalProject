49ac826f31c1977738d2b03cd3280fe98fa58a72
Sophie <tengxxu@gmail.com>
2021-07-26 15:13:21 -0400

    Add imporving the previous seaching function

--- DogBreed/DogBreed/Pages/Index.cshtml.cs
+++ DogBreed/DogBreed/Pages/Index.cshtml.cs
@@ -24,6 +24,12 @@ namespace DogBreed.Pages
             get; protected set;
         } = false;
 
+        //Use to not show ""
+        public bool IfSubmit 
+        {
+            get; set;
+        } = false;
+
         public IndexModel(ILogger<IndexModel> logger, DogBreed.Data.DogBreedDbContext context)
         {
             _logger = logger;
@@ -33,52 +39,76 @@ namespace DogBreed.Pages
 
         public void OnGet()
         {
-
+            IfSubmit = false;
         }
 
         private readonly Data.DogBreedDbContext _context;
 
         public IList<Breed> Breeds { get; set; }
 
-        public List<string> BreedList = new List<string>();
+        public List<string> MainBreedList = new List<string>();
 
         public List<string> SubBreedList = new List<string>();
 
         public List<string> ResultList = new List<string>();
 
 
-        public async Task OnPostAsync(string searchValue, string breedType)
+        private static bool CheckIfContain(string BreedName, string SearchValue) {
+            if (!String.IsNullOrEmpty(BreedName) && !String.IsNullOrEmpty(SearchValue) && BreedName.ToLower().Contains(SearchValue.ToLower()))
+            {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
+        public async Task OnPostAsync(string SearchValue, string BreedType)
         {
+            IfSubmit = true;
 
             Breeds = await _context.Breeds.ToListAsync();
 
-            BreedList = Breeds.Select(breed => breed.BreedName).ToList();  // The list of all main breed names
+            MainBreedList = Breeds.Select(breed => breed.BreedName).ToList();  // The list of all main breed names
             SubBreedList = Breeds.Select(breed => breed.SubBreedName).ToList();  // The list of all sub breed names
-            var AllBreedList = BreedList.Zip(SubBreedList, (breed, subBreed) => $"{breed} {subBreed}");  //The list of all (main breed name + sub breed name)s
+            var AllBreedList = MainBreedList.Zip(SubBreedList, (breed, subBreed) => $"{breed} {subBreed}");  //The list of all (main breed name + sub breed name)s
 
-            switch (breedType)
+            switch (BreedType)
             {
                 case "both":
-                    // search what user typed in the list
+                    // search the dog breed in the list
                     foreach (var AllBreed in AllBreedList)
                     {
-                        if (AllBreed.Contains(searchValue)){
-                            // TODO upper case and lower case
+                        if (CheckIfContain(AllBreed, SearchValue))
+                        {
                             ResultList.Add(AllBreed);
                         }
                     }
-
-                    
-                    if (ResultList.Count != 0) {
-                        ResultSet = true;
-                    }
                     break;
                 case "main_breed":
+                    foreach (var MainBreed in MainBreedList)
+                    {
+                        if (CheckIfContain(MainBreed, SearchValue))
+                        {
+                            ResultList.Add(MainBreed);
+                        }
+                    }
                     break;
                 case "sub_breed":
+                    foreach (var SubBreed in SubBreedList)
+                    {
+                        if (CheckIfContain(SubBreed, SearchValue))
+                        {
+                            ResultList.Add(SubBreed);
+                        }
+                    }
                     break;
                 default:
                     break;
+            }
+            if (ResultList.Count != 0)
+            {
+                ResultSet = true;
             }
 
             //return RedirectToPage("./Index");
