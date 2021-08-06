4aa6ce228a6b832c331a7e6e145d0ed3e20a3760
Sophie <tengxxu@gmail.com>
2021-08-06 17:16:08 -0400

    Add fetching data from Dog API and saving into database

---
+++ DogBreed/DogBreed/Models/BreedListResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogBreed.Models
+{
+    public class BreedListResponse
+    {
+        public Dictionary<string, string[]> message { get; set; }
+
+        public string status { get; set; }
+    }
+}
--- DogBreed/DogBreed/Pages/BreedList/Index.cshtml.cs
+++ DogBreed/DogBreed/Pages/BreedList/Index.cshtml.cs
@@ -7,23 +7,51 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using DogBreed.Data;
 using DogBreed.Models;
+using Newtonsoft.Json;
 
 namespace DogBreed.Pages.BreedList
 {
     public class IndexModel : PageModel
     {
-        private readonly DogBreed.Data.DogBreedDbContext _context;
+        private readonly DogBreedDbContext _context;
 
-        public IndexModel(DogBreed.Data.DogBreedDbContext context)
+        private string dogUrl = "https://dog.ceo/api/breeds/list/all";
+
+        public IndexModel(DogBreedDbContext context)
         {
             _context = context;
         }
 
-        public IList<Breed> Breed { get;set; }
+        public IList<Breed> Breeds { get;set; }
 
         public async Task OnGetAsync()
         {
-            Breed = await _context.Breeds.ToListAsync();
+            Breeds = await _context.Breeds.ToListAsync();
+
+            if (Breeds.Count == 0) {
+                string json = new System.Net.WebClient().DownloadString(dogUrl);
+                BreedListResponse breedResponse = JsonConvert.DeserializeObject<BreedListResponse>(json);
+                foreach (var item in breedResponse.message)
+                {
+                    var mainBreed = item.Key;
+                    foreach (var subBreed in item.Value)
+                    {
+                        Breed a = new Breed()
+                        {
+                            BreedName = mainBreed,
+                            SubBreedName = subBreed
+                        };
+                        _context.Breeds.Add(a);
+
+                    }
+                };
+
+                await _context.SaveChangesAsync();
+
+                Breeds = await _context.Breeds.ToListAsync();
+            }
+
         }
+
     }
 }
