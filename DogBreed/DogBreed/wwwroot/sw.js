6808584942b009b7e0e5b4c89d815041441ab975
Sophie <tengxxu@gmail.com>
2021-08-12 13:56:46 -0400

    Add manifest, service workers and cache for Web APP

---
+++ DogBreed/DogBreed/wwwroot/sw.js
@@ -0,0 +1,25 @@
+﻿const cacheName = 'cache-v1';
+const dynamicCache = 'dyn-cache-v1';
+let urlsToCache = [
+    '/',
+    '/css/site.css',
+    '/css/style.css'
+];
+
+self.addEventListener('install', (event) => {
+    console.info('Service Worker Installing...');
+
+    event.waitUntil(
+        caches.open(cacheName)
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+
+    console.info('Caching Started...');
+});
+
+
+
+ 
