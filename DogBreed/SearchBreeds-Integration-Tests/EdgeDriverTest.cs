4cec7028e3dc8be2b8a0033e670ab6794d674a20
Sophie <tengxxu@gmail.com>
2021-08-18 19:47:12 -0400

    Add three integration tests

--- DogBreed/SearchBreeds-Integration-Tests/EdgeDriverTest.cs
+++ DogBreed/SearchBreeds-Integration-Tests/EdgeDriverTest.cs
@@ -18,7 +18,6 @@ namespace SearchBreeds_Integration_Tests
         private ChromeDriver _driver;
         private string testSearchUrl = "http://localhost:5000/";
         private string testIndexUrl = "http://localhost:5000/BreedList";
-        private string testImageUrl = "http://localhost:5000/BreedList/ShowImage?id=158";
 
         [TestInitialize]
         public void EdgeDriverInitialize()
@@ -53,6 +52,46 @@ namespace SearchBreeds_Integration_Tests
         }
 
         [DataTestMethod]
+        [DataRow("terrier", "both", "terrier")]
+        [DataRow("sp", "main_breed", "")]
+        [DataRow("australian", "sub_breed", "Australian")]
+        public void VerifySearchResultError(string searchValue, string searchType, string expectedResult)
+        {
+            _driver.Url = testSearchUrl;
+            var searchValueBox = _driver.FindElementById("search_value");
+            var searchTypeBox = _driver.FindElementById(searchType);
+            var searchButton = _driver.FindElementById("search");
+
+            searchValueBox.SendKeys(searchValue);
+            searchTypeBox.Click();
+            searchButton.Click();
+
+            var output = _driver.FindElementById("search_result");
+            var answer = output.Text;
+
+            Assert.AreNotEqual(expectedResult, answer);
+        }
+
+        [DataTestMethod]
+        [DataRow("Both")]
+        [DataRow("")]
+        [DataRow("main breed")]
+        public void VerifySearchTypeError(string searchType)
+        {
+            _driver.Url = testSearchUrl;
+            object searchTypeBox = null;
+            try
+            {
+               searchTypeBox = _driver.FindElementById(searchType);
+            }
+            catch {
+                searchTypeBox = null;
+            }
+            Assert.IsNull(searchTypeBox);
+        }
+
+
+        [DataTestMethod]
         [DataRow("australian")]
         [DataRow("shepherd")]
         public void VerifyIndexResult(string expectedMainBreed)
@@ -61,6 +100,18 @@ namespace SearchBreeds_Integration_Tests
             var output = _driver.FindElementById("breed_table");
             var answer = output.Text;
             StringAssert.Contains(answer, expectedMainBreed);
+        }
+
+
+        [DataTestMethod]
+        [DataRow("Australian")]
+        [DataRow("Shepherd")]
+        public void VerifyIndexResultError(string expectedMainBreed)
+        {
+            _driver.Url = testIndexUrl;
+            var output = _driver.FindElementById("breed_table");
+            var answer = output.Text;
+            Assert.IsFalse(answer.Contains(expectedMainBreed), "");
         }
 
 
